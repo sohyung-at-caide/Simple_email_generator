598be3ef336e7bd18bbcbe63824277361035e398
Sohyo <cienakim12@gmail.com>
2024-04-24 12:14:30 +0200

    update run_baseline.sh to run baseline_cvs_hf.py

--- run_baseline.sh
+++ run_baseline.sh
@@ -3,5 +3,9 @@
 # Change to the directory containing the Python script
 cd ./
 
-# Run the Python script
-python3 baseline_email_generator_huggingface.py --sys_prompt seb_example --user_prompt seb_example --config_name openchat-3-5-extended
+# Run the baseline_email_generator_hf.py
+#python3 baseline_email_generator_hf.py --sys_prompt seb_example --user_prompt seb_example --config_name openchat-3-5-extended
+
+# Run the baseline_cvs_hf.py
+#python3 baseline_cvs_hf.py --csv_path Prompt_benchmarking_zero-shot.csv --config_name openchat-3-5-extended
+python3 baseline_cvs_hf.py --csv_path Prompt_benchmarking-few-shot.csv --config_name openchat-3-5-extended
